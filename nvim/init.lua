f3f27d19a3dc79c40a4caddd79e32bb42d393b61
Doug Woodward <doug@fourthpin.com>
2024-06-09 15:17:03 -0700

    revamped dot files

---
+++ nvim/init.lua
@@ -0,0 +1,25 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup("plugins")
+
+
+
---
+++ nvim/lua/plugins/nvim_tree.lua
@@ -0,0 +1,22 @@
+
+local M = {
+  "nvim-tree/nvim-tree.lua",
+}
+
+M.config = function() 
+  require("nvim-tree").setup({
+	  sort = {
+	    sorter = "case_sensitive",
+	  },
+	  view = {
+	    width = 30,
+	  },
+	  renderer = {
+	    group_empty = true,
+	  },
+	  filters = {
+	    dotfiles = true,
+	  },
+  })
+end
+return M
