bb1c809999bb807293816bb482703fbd199eee29
Doug Woodward <doug@fourthpin.com>
2024-06-09 17:39:39 -0700

    add updated tmux and always open nvim tree

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -18,5 +18,35 @@ M.config = function()
 	    dotfiles = true,
 	  },
   })
+local nt_api = require("nvim-tree.api")
+
+-- Set the default state to closed
+local tree_open = false
+
+-- Function to toggle the tree open or closed
+local function toggle_tree(state)
+  if state then
+    nt_api.tree.open()
+  else
+    nt_api.tree.close()
+  end
+end
+
+-- Subscribe to the TreeOpen and TreeClose events
+nt_api.events.subscribe(nt_api.events.Event.TreeOpen, function()
+  tree_open = true
+end)
+
+nt_api.events.subscribe(nt_api.events.Event.TreeClose, function()
+  tree_open = false
+end)
+
+-- Set up an autocmd to toggle the tree open or closed when switching tabs
+vim.api.nvim_create_autocmd("TabEnter", {
+  callback = function()
+    toggle_tree(tree_open)
+  end
+})
+
 end
 return M
