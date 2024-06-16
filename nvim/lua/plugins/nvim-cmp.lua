d09ae5bd2eabba798e485449b1dda58db565ab09
Doug Woodward <doug@fourthpin.com>
2024-06-15 22:21:55 -0700

    update nvim-cmp behavior so that it does not use enter

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -15,30 +15,33 @@ local M = {
 }
 M.config = function()  
   -- Set up nvim-cmp.
+  vim.o.completeopt = "menuone,noselect,preview"
   local cmp = require'cmp'
 
   cmp.setup({
+    preselect = cmp.PreselectMode.None,
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+	vim.o.completeopt = "menuone,noselect,preview"
       end,
     },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
+    window = {},
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+      ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = function(fallback)
+        if cmp.visible() then
+          cmp.abort()
+        else
+          fallback()
+        end
+      end,
+     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'vsnip' }, -- For vsnip users.
@@ -91,7 +94,13 @@ M.config = function()
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = function(fallback) -- yes define agin here
+        if cmp.visible() then
+          cmp.abort()
+        else
+          fallback()
+        end
+     end,
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
